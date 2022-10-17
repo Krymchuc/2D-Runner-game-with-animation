87399d1474029297cc1c31b28f3c2dcac771a4ab
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-10-17 21:25:54 +0300

    Sticky platforms and dash

--- Running game 2D New Input System/Assets/RunnerMovement.cs
+++ Running game 2D New Input System/Assets/RunnerMovement.cs
@@ -11,19 +11,30 @@ public class RunnerMovement : MonoBehaviour
     private Rigidbody2D rb;
     public Animator animator;
     public bool facingRight=true;
-    // Start is called before the first frame update
+
+    //dash
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+    [SerializeField] private TrailRenderer trailRenderer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         move = Input.GetAxis("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(move));
 
-        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+        //rb.velocity = new Vector2(speed * move, rb.velocity.y);
         if(facingRight==false && move>0){
             Flip();
         }
@@ -35,7 +46,21 @@ public class RunnerMovement : MonoBehaviour
             rb.AddForce(new Vector2(rb.velocity.x, jump));
             Debug.Log("jump");
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+    }
+
     private void OnCollisionEnter2D(Collision2D item)
     {
         if (item.gameObject.CompareTag("Ground"))
@@ -57,6 +82,22 @@ public class RunnerMovement : MonoBehaviour
         Vector3 scaler=transform.localScale;
         scaler.x*=-1;
         transform.localScale=scaler;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        trailRenderer.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
 
